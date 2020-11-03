40e697597f6cd441e01c0789a3d0212801090ad0
BeatriceAsplund <73177854+BeatriceAsplund@users.noreply.github.com>
2020-11-03 16:11:21 +0100

    Update app.js

--- MicroTask1_GreetingCard/app.js
+++ MicroTask1_GreetingCard/app.js
@@ -3,6 +3,10 @@ const backgroundColorElement = document.getElementById("backgroundColor");
 const fontElement = document.getElementById("font");
 const fontColorElement = document.getElementById("fontColor");
 const fontSizeElement = document.getElementById("fontSize");
+
+const borderTypeElement = document.getElementById("borderType");
+const borderColorElement = document.getElementById("borderColor");
+
 const outputElement = document.getElementById("output");
 const cardElement  = document.getElementById("card");
 const buttonElement = document.getElementById("btnProcess");
@@ -13,6 +17,12 @@ function greetingCard() {
 
     const backgroundColor = backgroundColorElement.value;
     cardElement.style.backgroundColor = backgroundColor;
+
+    const borderType = borderTypeElement.value;
+    cardElement.style.borderStyle = borderType;
+
+    const borderColor = borderColorElement.value;
+    cardElement.style.borderColor = borderColor;
 
     const font = fontElement.value;
     outputElement.style.fontFamily = font;
